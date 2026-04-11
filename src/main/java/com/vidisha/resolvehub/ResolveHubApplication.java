207da6ad8e95a47f767fbecfc0d38119374ec422
vidishaa9 <vidishaarora14@gmail.com>
2026-04-11 12:04:55 +0530

    init commit

---
+++ src/main/java/com/vidisha/resolvehub/ResolveHubApplication.java
@@ -0,0 +1,13 @@
+package com.vidisha.resolvehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolveHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResolveHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vidisha/resolvehub/ResolveHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidisha.resolvehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResolveHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
