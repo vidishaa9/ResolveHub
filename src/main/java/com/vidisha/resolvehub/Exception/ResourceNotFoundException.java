cea98551e2a033417fe917c4e6cbf1ab3b95d661
vidishaa09 <vidishaarora14@gmail.com>
2026-04-12 23:06:28 +0530

    Error Handling

---
+++ src/main/java/com/vidisha/resolvehub/Exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.vidisha.resolvehub.Exception;
+
+
+/**
+ * Custom exception when resource is not found
+ */
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
+
+
+
+
+
