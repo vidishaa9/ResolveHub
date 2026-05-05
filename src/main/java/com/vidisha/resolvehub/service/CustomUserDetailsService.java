9a3fba5c8b504c5ab7d100041fef96cd1efc36bb
vidishaa09 <vidishaarora14@gmail.com>
2026-05-05 10:54:07 +0530

    customerdetailsservice

---
+++ src/main/java/com/vidisha/resolvehub/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.vidisha.resolvehub.service;
+
+import com.vidisha.resolvehub.model.User;
+import com.vidisha.resolvehub.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+/**
+ * Loads user from DB for Spring Security
+ */
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().replace("ROLE_", ""))
+                .build();
+    }
+}
